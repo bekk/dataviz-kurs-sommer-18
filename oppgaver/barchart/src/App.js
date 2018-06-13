f7ee4f86c461b7b9bdef4c53e2fd89c8ddcb77c5
Stian Veum Møllersen <stian.moellersen@gmail.com>
2018-06-13 12:00:37 +0200

    Reorg til lerna-monorepo

---
+++ oppgaver/barchart/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import BarChart from './BarChart';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>d3ia dashboard</h2>
+        </div>
+        <div>
+          <BarChart data={[5, 10, 1, 3]} size={[500, 500]} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
